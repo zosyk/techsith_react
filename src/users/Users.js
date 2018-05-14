3f67a435614045659c7a7c7d3efcb458f9f81e1e
zosyk <alekcandrzosyk@gmail.com>
2018-05-14 22:59:46 +0300

    added state/ setState behaviour

--- src/users/Users.js
+++ src/users/Users.js
@@ -3,13 +3,39 @@ import User from "./User"
 
 
 class Users extends Component {
+
+    state = {
+        users: [
+            {name: "John", age: 20},
+            {name: "Peter", age: 21}
+        ],
+        title: "User List12"
+    };
+
+    makeMeYounger = () => {
+        const newState = this.state.users.map((user) => {
+            if (user.age > 10) {
+                user.age -= 10;
+            }
+
+            return user;
+        });
+
+        this.setState(newState)
+    };
+
     render() {
         return (
             <div>
-                <h1>{this.props.title}</h1>
-                <User age={20}>John</User>
-                <User></User>
-                <User age="25">Peter</User>
+                <br/>
+                <button onClick={this.makeMeYounger}>Make us 10 years younger</button>
+                <br/>
+                <h1>{this.state.title}</h1>
+                {
+                    this.state.users.map((user) => {
+                        return <User age={user.age}>{user.name}</User>
+                    })
+                }
             </div>
         )
     }
