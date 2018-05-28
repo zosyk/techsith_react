959429c4456b11f8e8ec07d12270d843f1b2299e
zosyk <alekcandrzosyk@gmail.com>
2018-05-28 11:15:50 +0300

    added logic of using Fragment components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import './App.css';
 import User from "./users/User";
 import  UniqueId from "react-html-id";
+
+const Temp  = (props) => {
+    return (
+        <Fragment>
+
+            {
+                props.greetings === 'hi' ?
+                    <Fragment>&lt;div&gt;{props.greetings}&lt;/div&gt;</Fragment> :
+                    <h1>{props.greetings}</h1>
+            }
+
+        </Fragment>
+    )
+};
 
 class App extends Component {
 
@@ -39,27 +53,12 @@ class App extends Component {
         users[index] = user;
 
         this.setState({users:users});
-    }
+    };
 
     render() {
         return (
             <div className="App">
-                <ul>
-                    {
-                        this.state.users.map((user, index) => {
-
-                            return <User
-                                key={user.id}
-                                age={user.age}
-                                deleteEvent={this.deleteUser.bind(this, index)}
-                                changeEvent={this.changeUserName.bind(this, user.id)}
-                                >
-                                    {user.name}
-                                </User>
-                        })
-                    }
-                </ul>
-
+                <Temp greetings="hi"/>
             </div>
         );
     }
