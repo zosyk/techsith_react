224e9c53a40e17464713d4d82f0465058db9abf9
zosyk <alekcandrzosyk@gmail.com>
2018-06-11 22:48:26 +0300

    added React Router (Route, Link, NavLink, Redirect, Prompt )

--- src/App.js
+++ src/App.js
@@ -1,111 +1,83 @@
 import React, {Component, Fragment} from 'react';
 import './App.css';
 import Child from './components/parentToChild/Child';
+import {BrowserRouter as Router, Link, NavLink, Redirect, Prompt} from "react-router-dom";
+import Route from "react-router-dom/Route";
 
-const Temp  = (props) => {
+const User = (params) => {
     return (
-        <Fragment>
-
-            {
-                props.greetings === 'hi' ?
-                    <Fragment>&lt;div&gt;{props.greetings}&lt;/div&gt;</Fragment> :
-                    <h1>{props.greetings}</h1>
-            }
-
-        </Fragment>
+        <h1>Welcome User {params.username}</h1>
     )
 };
 
+
 class App extends Component {
 
-    constructor() {
-        super();
-
-        this.state = {
-            name: "john"
-        };
-
-        console.log("constructor");
-    }
-
-    componentWillMount() {
-        console.log("componentWillMount", window.innerWidth);
-
-    }
-
-
-    componentDidMount(){
-        console.log("componentDidMount");
-
-    }
-
-    componentWillReceiveProps() {
-        console.log("componentWillReceiveProps");
-
-    }
-
-
-    shouldComponentUpdate(nextProps, nextState) {
-        console.log("shouldComponentUpdate");
-        return true;
-    }
-
-    componentWillUpdate() {
-        console.log("componentWillUpdate");
-    }
-
-    componentDidUpdate(prevProps, prevState) {
-        console.log("componentDidUpdate");
-    }
-
-    componentWillUnmount() {
-        console.log("componentWillUnmount");
-    }
-
-
-
-    deleteUser = (index, event) => {
-        const users = Object.assign([], this.state.users);
-        users.splice(index, 1);
-
-        this.setState({users:users});
+    state={
+        loggedIn:false
     };
 
-    changeUserName = (id, e) => {
-        const index = this.state.users.findIndex((user) => {
-            return user.id === id;
-        });
-        const user = Object.assign({}, this.state.users[index]);
-        user.name = e.target.value;
-        const users = Object.assign([], this.state.users);
-        users[index] = user;
-
-        this.setState({users:users});
+    loginHandle =() =>  {
+      this.setState(prevState => (
+          {
+              loggedIn: !prevState.loggedIn
+          }
+      ))
     };
-
-    changeState = () => {
-        this.setState({name:"Hill"})
-    };
-
-    unmountChild() {
-        this.setState({name:"robert"});
-    }
 
     render() {
 
-        console.log("render");
-
-        if(this.state.name === 'robert') {
-            return (<div/>);
-        }
-
         return (
-            <div className="App">
-                <div>parent name: {this.state.name}</div>
-                <Child name={this.state.name}/>
-                <button onClick={this.changeState.bind(this)}>Change state</button>
-                <button onClick={this.unmountChild.bind(this)}>Unmount child</button>
-            </div>
+
+            <Router>
+                <div className="App">
+
+                    <ul>
+                        <li>
+                            <NavLink exact activeStyle={
+                                {color:'green'}
+                            } to="/">Home</NavLink>
+                        </li>
+                        <li>
+                            <NavLink exact  activeStyle={
+                                {color:'green'}
+                            } to="/about/">About</NavLink>
+                        </li>
+                        <li>
+                            <NavLink exact activeStyle={
+                                {color:'green'}
+                            } to="/user/Steave">User Steave</NavLink>
+                        </li>
+                    </ul>
+
+                    <Prompt when={!this.state.loggedIn} message={
+                        (location)=> {
+                            return location.pathname.startsWith('/user') ? "Are you sure?" : true
+                        }
+                    }/>
+                    <input type="button" onClick={this.loginHandle.bind(this)} value={this.state.loggedIn ? "Log out" : "Log in"}/>
+
+                    <Route path="/" exact strict={true} render={
+                        () => {
+                            return (
+                                <h1>Welcome Home</h1>
+                            )
+                        }
+                    }/>
+                    <Route path="/about/" exact={true} strict render={
+                        () => {
+                            return (
+                                <h1>Welcome About</h1>
+                            )
+                        }
+                    }/>
+
+                    <Route path="/user/:username" exact={true} strict render={({match}) => {
+                        return this.state.loggedIn ? (<User username={match.params.username}/>) : (<Redirect to="/"/>)}
+                    }/>
+
+                </div>
+            </Router>
         );
     }
 }
